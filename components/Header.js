fcc5e352f0ff43a552293bed625416656a5a91dd
Anna <a.solilee@gmail.com>
2022-08-16 19:45:12 -0700

    Added styling to landing page

--- components/Header.js
+++ components/Header.js
@@ -24,31 +24,26 @@ export default function Header() {
   }
 
   return (
-    <div className="flex p-4 justify-between text-white bg-[#B6B29D] items-center border-b-[0.5px] border-gray-600">
-      <button className="sm:hidden" aria-label="menu icon">
-        {openMenu ?
-          <MenuIcon
-            onClick={openNav}
-            id="open-menu-item"
-            className="w-6 h-6 hover:pointer relative"/>:
-          <XIcon
-            onClick={closeNav}
-            id="close-menu-icon"
-            className="w-6 h-6 hover:pointer text-black z-50 relative"/>
-        }
-      </button>
+    <div className="flex p-12 sm:p-8 justify-between items-center border-gray-600 w-full">
+      {openMenu ?
+        <MenuIcon
+          onClick={openNav}
+          id="open-menu-item"
+          className="w-6 h-6 hover:pointer relative text-white sm:invisible"/>:
+        <XIcon
+          onClick={closeNav}
+          id="close-menu-icon"
+          className="w-6 h-6 hover:pointer text-black z-50 relative"/>
+      }
+
       <Link href="/">
-        <div className="font-semibold z-30 text-2xl hover:cursor-pointer">
+        <div className="font-semibold z-30 text-2xl hover:cursor-pointer text-white ml-[2em] sm:ml-0 sm:mr-auto">
           Mazesoba
         </div>
       </Link>
+
       <nav>
-        <ul className="hidden fixed bg-white text-black sm:text-white w-full z-10 left-0 top-0 bottom-0 sm:bg-transparent flex flex-col sm: flex-start sm:inline-flex sm:flex-row absolute sm:static sm:h-fit sm:w-fit items-center justify-center sm:justify-between text-2xl sm:text-lg sm:w-[18em]" id="menu">
-          <li className="hover:text-blue-400 hover:cursor-pointer tracking-wider">
-            <Link href="/">
-              <a onClick={closeNav}>Home</a>
-            </Link>
-          </li>
+        <ul className="hidden fixed bg-white text-black sm:text-white w-full z-10 left-0 top-0 bottom-0 sm:bg-transparent flex flex-col sm: flex-start sm:inline-flex sm:flex-row absolute sm:static sm:h-fit sm:w-fit items-center justify-center sm:justify-between text-2xl sm:text-lg sm:w-[18em] sm:mr-8" id="menu">
           <li className="hover:text-blue-400 hover:cursor-pointer tracking-wider">
             <Link href="/order">
               <a onClick={closeNav}>Order</a>
@@ -64,16 +59,21 @@ export default function Header() {
               <a onClick={closeNav}>Our Story</a>
             </Link>
           </li>
+          <li className="hover:text-blue-400 hover:cursor-pointer tracking-wider">
+            <Link href="/contact">
+              <a onClick={closeNav}>Contact</a>
+            </Link>
+          </li>
         </ul>
       </nav>
 
       <Link href="/cart">
         {total > 0?
         <SolidCartIcon
-          className="h-6 w-6 text-red-800 hover:text-blue-800 hover:cursor-pointer"
+          className="h-6 w-6 text-white hover:cursor-pointer"
         />:
         <ShoppingCartIcon
-          className="h-6 w-6 hover:cursor-pointer"
+          className="h-6 w-6 text-white hover:cursor-pointer"
         />
         }
       </Link>
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 import Header from '../components/Header';
-import MainImage from '../public/note-thanun-unsplash.jpg';
+import MainImage from '../public/mitya-ivanov.jpg';
 import Link from 'next/link';
 
 export default function Home() {
@@ -14,18 +14,15 @@ export default function Home() {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
 
-      <Header/>
-      <main>
-        <section className="relative h-screen w-full">
-          <Image
-            src={MainImage} objectFit="cover" layout="fill"
-            alt="closeup of hot soba"
-          />
+      <main className="flex flex-col items-center sm:items-start bg-main-image h-screen">
+        <Header/>
+
+          <div className="sm:text-start text-center text-white sm:ml-12 mt-48 text-5xl sm:max-w-md">Traditional Japanese buckwheat noodles, made entirely from scratch.</div>
           <Link href="/order">
             <button
-              className="absolute text-white text-lg py-2 px-4 bg-[#B6B29D] opacity-80 top-1/2 left-1/3 sm:left-[43%] md:left-[45%]">Place Order</button>
+              className="uppercase mt-12 sm:ml-12 bg-white text-gray-800 text-lg py-2 px-4">Order now</button>
           </Link>
-        </section>
+
       </main>
 
     </div>
